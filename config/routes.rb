ef352daa97edf371f5f5d8ca7dfa395c52ebea72
tHasegawa13 <thsgw13.stac@gmail.com>
2021-06-13 07:08:31 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'static_pages#home'
+  get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/about', to: 'static_pages#about'
+  get 'static_pages/contact', to: 'static_pages#contact'
 end
