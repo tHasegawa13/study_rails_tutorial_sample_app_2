1dc6b742d744ee796f8273ecab70a881033294e7
tHasegawa13 <thsgw13.stac@gmail.com>
2021-06-18 04:21:11 +0000

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'c05bdf282d374acf86f34365c9ad946e.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210617034250) do
+ActiveRecord::Schema.define(version: 20210618021856) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20210617034250) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
